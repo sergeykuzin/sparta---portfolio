5c56c5cbb0d5dc6f3008b49b78330b06b72480f5
sergeykuzin <sergeykuzin2008@ya.ru>
2020-03-05 15:01:48 +0300

    feat: added a slider for the reviews section

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,11 @@
+/*
+ * Youtub api
+ */
 const onLoadWindow = () => {
   const playButton = document.querySelector('.video-player__play-button');
   let player = null;
 
-  window.console.log('YT - ', YT);
+  // window.console.log('YT - ', YT);
 
   player = new YT.Player('player', {
     videoId: 'NR2pM95f8Rw',
@@ -20,3 +23,108 @@ const onLoadWindow = () => {
 // window.attachEvent ? window.attachEvent('onload', onLoadWindow) : window.addEventListener('load', onLoadWindow);
 
 window.addEventListener('load', onLoadWindow);
+
+/*
+ * Slider
+ */
+
+const reviewTextWrapper = document.querySelector('.testimonials__item-reviewer-text');
+const reviewTextButton = document.querySelector('.testimonials__toggle-text');
+
+reviewTextButton.addEventListener('click', () => {
+  if (!reviewTextWrapper.style.overflow || reviewTextWrapper.style.overflow === 'hidden') {
+    reviewTextWrapper.style.overflow = 'auto';
+  } else {
+    reviewTextWrapper.style.overflow = 'hidden';
+  }
+});
+
+const nextSlideButton = document.querySelector('.testimonials__pagination-right');
+const prevSlideButton = document.querySelector('.testimonials__pagination-left');
+const slides = document.querySelectorAll('.testimonials__item');
+const slidesWrapper = document.querySelector('.testimonials__list');
+const dots = document.querySelectorAll('.testimonials__pagination-dot');
+
+const moveSlides = () => {
+  const slideWidht = 1140;
+  slides.forEach((slide, index) => {
+    if (slide.dataset.slideActive === 'true') {
+      const activeSlideIndex = index;
+      const slideMove = slideWidht * activeSlideIndex;
+      slidesWrapper.style.transform = `translateX(-${slideMove}px)`;
+    }
+  });
+};
+
+const moveDots = () => {
+  console.log('moveDots() - start');
+  let activeSlideIndex = null;
+
+  slides.forEach((slide, index) => {
+    if (slide.dataset.slideActive === 'true') {
+      activeSlideIndex = index;
+    }
+  });
+
+  dots.forEach((dot) => {
+    if (dot.dataset.dotActive === 'true') {
+      dot.dataset.dotActive = 'false';
+      dot.classList.remove('testimonials__pagination-dot--active');
+      dots[activeSlideIndex].dataset.dotActive = 'true';
+      dots[activeSlideIndex].classList.add('testimonials__pagination-dot--active');
+    }
+  });
+};
+
+const onClickNextSlideButton = () => {
+  let isSkipIteration = false;
+
+  slides.forEach((slide, index) => {
+    if (isSkipIteration) {
+      return;
+    }
+    if (slide.dataset.slideActive === 'true') {
+      slide.dataset.slideActive = 'false';
+
+      if (index === slides.length - 1) {
+        slides[0].dataset.slideActive = 'true';
+        isSkipIteration = true;
+        return;
+      }
+
+      slide.nextElementSibling.dataset.slideActive = 'true';
+      isSkipIteration = true;
+    }
+  });
+
+  moveSlides();
+  moveDots();
+};
+
+const onClickPrevSlideButton = () => {
+  let isSkipIteration = false;
+
+  slides.forEach((slide, index) => {
+    if (isSkipIteration) {
+      return;
+    }
+    if (slide.dataset.slideActive === 'true') {
+      slide.dataset.slideActive = 'false';
+
+      if (index === 0) {
+        slides[slides.length - 1].dataset.slideActive = 'true';
+        isSkipIteration = true;
+        return;
+      }
+
+      slide.previousElementSibling.dataset.slideActive = 'true';
+      isSkipIteration = true;
+    }
+  });
+
+  moveSlides();
+  moveDots();
+};
+
+nextSlideButton.addEventListener('click', onClickNextSlideButton);
+prevSlideButton.addEventListener('click', onClickPrevSlideButton);
