b9c366388e27751308df064a13a577065aa01422
sergeykuzin <sergeykuzin2008@ya.ru>
2020-03-21 10:54:58 +0300

    build: changed assembly system configs on a simpler

--- gulpfile.js
+++ gulpfile.js
@@ -1,198 +1,140 @@
+'use strict';
+
 const gulp = require('gulp');
-// SCSS => CSS
 const sass = require('gulp-sass');
-// PUG => HTML
-const pug = require('gulp-pug');
-// CSS vendor prefixes
 const autoprefixer = require('gulp-autoprefixer');
-// CSS minification
 const csso = require('gulp-csso');
-// Create a raster sprite
-const spritesmith = require('gulp.spritesmith');
-// Create SVG sprite
-const svgSprite = require('gulp-svg-sprite');
-// Converting images to webp
-const webp = require('gulp-webp');
-// Add hash to file name
-const rev = require('gulp-rev');
-// Delete the original file rewritten by gulp-rev.
-const revdel = require('gulp-rev-delete-original');
-// Replacing HTML links for CSS with new CSS names with hashes
-const revCollector = require('gulp-rev-collector');
-// Deleting files / directories
+const pug = require('gulp-pug');
+const webpack = require('webpack-stream');
+const browsersync = require('browser-sync');
 const del = require('del');
-// Hot reload
-const browserSync = require('browser-sync').create();
 
+const dist = './dist/';
 
-const pugConfig = { pretty: true };
+gulp.task('buildHtml', () => {
+  return gulp
+    .src('./src/pug/pages/*.pug')
+    .pipe(pug({ pretty: true }))
+    .pipe(gulp.dest(dist))
+    .pipe(browsersync.stream());
+});
 
-const spritesmithConfig = {
-  imgName: 'sprite.png',
-  cssName: 'sprite.scss',
-  cssFormat: 'scss',
-  algorithm: 'binary-tree',
-  imgPath: '../img/sprite/sprite.png',
-};
-
-// PUG => HTML
-function buildHtml() {
-  return gulp.src('./src/pug/pages/*.pug')
-    .pipe(pug(pugConfig))
-    .pipe(gulp.dest('./dist/'))
-    .pipe(gulp.dest('./src/template-html-for-webpack'))
-    .pipe(browserSync.stream());
-}
-exports.buildHtml = buildHtml;
-
-// SCSS => CSS => prefixes => minification
-function buildCss() {
-  return gulp.src([
-    './src/sass/core.scss',
-    './src/sass/pages/*.scss'])
+gulp.task('buildCss', () => {
+  return gulp
+    .src('./src/sass/pages/*.scss')
     .pipe(sass())
     .pipe(autoprefixer({
       cascade: false,
-      overrideBrowserslist: [
-        'IE 10',
-      ],
     }))
     .pipe(csso({ restructure: true }))
-    .pipe(gulp.dest('./dist/css'))
-    .pipe(browserSync.stream());
-}
-exports.buildCss = buildCss;
+    .pipe(gulp.dest(dist + 'css/'))
+    .pipe(browsersync.stream());
+});
 
-// Create a raster sprite
-function rasterSprite() {
-  return gulp.src('./src/img/icon-raster/*.png')
-    .pipe(spritesmith(spritesmithConfig))
-    .pipe(gulp.dest('./src/img/sprite/'));
-}
-exports.rasterSprite = rasterSprite;
+gulp.task('copyImage', () => {
+  return gulp
+    .src('./src/img/**/*')
+    .pipe(gulp.dest(dist + 'img/'));
+});
 
-// Create SVG sprite
-function vectorSprite() {
-  return gulp.src('./src/img/icon-vector/*.svg')
-    .pipe(svgSprite({
-      svg: {
-        xmlDeclaration: false,
-        doctypeDeclaration: false,
-      },
+gulp.task('copyFonts', () => {
+  return gulp
+    .src('./src/fonts/**/*')
+    .pipe(gulp.dest(dist + 'fonts/'));
+});
 
-      mode: {
+gulp.task('clean', () => del('./dist'));
 
-        symbol: {
-          bust: false,
-          dest: './',
-          sprite: './vector-sprite',
-          common: 'background-image',
+gulp.task('build-js', () => {
+  return gulp
+    .src('./src/js/main.js')
+    .pipe(
+      webpack({
+        mode: 'development',
+        output: {
+          filename: 'main.js',
         },
+        watch: false,
+        devtool: 'source-map',
+        module: {
+          rules: [
+            {
+              test: /\.m?js$/,
+              	exclude: /(node_modules|bower_components)/,
+              // exclude: /node_modules[\/\\](?!(swiper|dom7)[\/\\])/,
+              use: {
+                loader: 'babel-loader',
+                options: {
+                  presets: [
+                    [
+                      '@babel/preset-env',
+                      {
+                        debug: true,
+                        corejs: 3,
+                        useBuiltIns: 'usage'
+                      }
+                    ]
+                  ]
+                }
+              }
+            }
+          ]
+        }
+      })
+    )
+    .pipe(gulp.dest(dist + 'js/'))
+    .on('end', browsersync.reload);
+});
 
-      },
-    }))
-    .pipe(gulp.dest('./src/img/sprite'));
-}
-exports.vectorSprite = vectorSprite;
-
-// Converting images to webp
-function createWebp() {
-  return gulp.src([
-    './src/img/**',
-    '!./src/img/sprite/**',
-    '!./src/img/webp/**',
-    '!./src/img/icon-raster/**',
-    '!./src/img/icon-vector/**'], {
-    nodir: true,
-  })
-    .pipe(webp({ quality: 90 }))
-    .pipe(gulp.dest('./src/img/webp/'));
-}
-exports.createWebp = createWebp;
-
-// Add hash to file name
-function hashForCss() {
-  return gulp.src('./dist/css/*.css')
-    .pipe(rev())
-    .pipe(revdel())
-    .pipe(gulp.dest('./dist/css'))
-    .pipe(rev.manifest())
-    .pipe(gulp.dest('./src/manifest-for-hash-css'))
-    .pipe(gulp.src(['./src/manifest-for-hash-css/*.json', 'dist/*.html']));
-}
-exports.hashForCss = hashForCss;
-
-// Replacing HTML links for CSS with new CSS names with hashes
-function replaceCssHashLinkInHtml() {
-  return gulp.src([
-    'src/manifest-for-hash-css/*.json', 'dist/*.html'])
-    .pipe(revCollector({
-      replaceReved: true,
-      dirReplacements: {
-        css: 'css',
-      },
-    }))
-    .pipe(gulp.dest('dist'));
-}
-exports.replaceCssHashLinkInHtml = replaceCssHashLinkInHtml;
-
-// Copy source to production folder
-function copy() {
-  return gulp.src([
-    './src/fonts/**',
-    '!./src/img/icon-raster/*.png',
-    '!./src/img/icon-vector/*.svg',
-    './src/img/**/*.png',
-    './src/img/**/*.jpg',
-    './src/img/**/*.svg',
-    './src/img/**/*.webp',
-    './src/js/vendor/**',
-    './src/video/**',
-  ], {
-    base: './src/',
-  })
-    .pipe(gulp.dest('./dist/'));
-}
-exports.copy = copy;
-
-// Delete production folder
-function clean() {
-  return del('./dist');
-}
-exports.clean = clean;
-
-// Start server + watch for changes
-function serve() {
-  browserSync.init({
-    server: './dist',
-    notify: false,
+gulp.task('watch', () => {
+  browsersync.init({
+    server: './dist/',
+    port: 4000,
+    notify: true,
   });
 
-  gulp.watch('./src/sass/**/*.scss', buildCss);
-  gulp.watch('./src/pug/**/*.pug', buildHtml);
-  gulp.watch('./src/js/**/*.js').on('change', browserSync.reload);
-}
-exports.serve = serve;
+  gulp.watch('./src/pug/**/*.pug', gulp.parallel('buildHtml'));
+  gulp.watch('./src/sass/**/*.scss', gulp.parallel('buildCss'));
+  gulp.watch('./src/js/**/*.js', gulp.parallel('build-js'));
+  gulp.watch('./src/img/**/*', gulp.parallel('copyImage'));
+});
 
-// Build for development
-exports.dev = gulp.series(
-  clean,
-  rasterSprite,
-  buildHtml,
-  buildCss,
-  vectorSprite,
-  copy,
-);
+gulp.task('build', gulp.series('clean', 'buildHtml', 'buildCss', 'build-js', 'copyImage', 'copyFonts'));
 
-// Build for production
-exports.build = gulp.series(
-  clean,
-  rasterSprite,
-  buildHtml,
-  buildCss,
-  hashForCss,
-  vectorSprite,
-  replaceCssHashLinkInHtml,
-  createWebp,
-  copy,
-);
+gulp.task('build-prod-js', () => {
+  return gulp
+    .src('./src/js/main.js')
+    .pipe(
+      webpack({
+        mode: 'production',
+        output: {
+          filename: 'main.js'
+        },
+        module: {
+          rules: [
+            {
+              test: /\.m?js$/,
+              exclude: /(node_modules|bower_components)/,
+              use: {
+                loader: 'babel-loader',
+                options: {
+                  presets: [
+                    [
+                      '@babel/preset-env',
+                      {
+                        corejs: 3,
+                        useBuiltIns: 'usage'
+                      }
+                    ]
+                  ]
+                }
+              }
+            }
+          ]
+        }
+      })
+    )
+    .pipe(gulp.dest(dist));
+});
+
+gulp.task('default', gulp.parallel('watch', 'build'));
